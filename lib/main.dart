b53353c4688e35485d0799ea427f2c9323801e0b
Raghavendra <raghu.teegela@gmail.com>
2020-04-22 21:01:37 +0530

    Complete xylophone UI and Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -7,10 +10,40 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                getExpandedBtn(1, Colors.red),
+                getExpandedBtn(2, Colors.orangeAccent),
+                getExpandedBtn(3, Colors.yellow),
+                getExpandedBtn(4, Colors.green),
+                getExpandedBtn(5, Colors.teal),
+                getExpandedBtn(6, Colors.blue),
+                getExpandedBtn(7, Colors.purple),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  Expanded getExpandedBtn(int btnNum, Color btnColor) {
+    return Expanded(
+      child: FlatButton(
+        color: btnColor,
+        onPressed: () {
+          playAudio(btnNum);
+        },
+      ),
+    );
+  }
+
+  void playAudio(int buttonNo) {
+    final player = AudioCache();
+    player.play('note$buttonNo.wav');
   }
 }
